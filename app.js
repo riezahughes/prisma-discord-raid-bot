5fc4ea2c562179ded65c80832d587f8410bb41b0
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2020-03-04 23:01:54 +0000

    setting up sanitisation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const axios = require('axios');
+const xios = require('axios');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -10,29 +10,58 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === '!$leader') {
+
+  let sanatisedMessage = msg.content.toLowerCase();
+
+  if (sanatisedMessage.includes('!$leader') === true) {
     // Check to see if db already has this person registered. If not, register to the bot as a leader.
+    msg.reply(`You are now Leader`);
   }
-  if (msg.content === '!$createparty') {
+
+  if (sanatisedMessage.includes('!$passlead') === true) {
+    // pass the leader to someone else.
+    msg.reply(`You have passed the lead to someone else`);
+  }
+
+  if (sanatisedMessage.includes('!$createparty') === true) {
     // Creates Party for the leader who asks. Use a nickname to quick reference maybe? 
+    msg.reply(`Party Created!`);
   }
-  if (msg.content === '!$removeparty') {
+
+  if (sanatisedMessage.includes('!$removeparty') === true) {
     // delete party for leader who asks. Offer a prompt for it.
+    msg.reply('Party Blown Up');
   }
-  if (msg.content === '!$viewparty') {
+
+  if(sanatisedMessage.includes('!$clearparty') === true){
+    msg.reply(`Party is empty`)
+  }
+
+  if (sanatisedMessage.includes('!$viewparty') === true) {
     // Check to see if db already has this person registered. If not, register to the bot as a leader.
+    msg.reply(` See Party List `)
   }
-  if (msg.content === '!$addmember') {
-    // Adds a member to the party. 
+
+  if (sanatisedMessage.includes('!$addmember') === true) {
+    msg.reply(`A member has been added to the party`)
   }
+
   if (msg.content === '!$removeme') {
+    msg.reply(`You've removed yourself from the party.`)
     // remove member from party. checks if they are a member or sub.
   }
+
   if (msg.content === '!$addsub') {
     // Adds an Available Sub to the Party. 
+    msg.reply(`You've added a sub.`)
   }
+
   if (msg.content === '!$removesub'){
-    // Remove sub from party.
+    msg.reply(`Sub has been removed.`)
+  }
+
+  if (msg.content === `!$clearsubs`){
+    msg.reply(`All subs cleared`);
   }
 
 });
